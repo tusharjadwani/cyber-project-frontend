5dc17b4fb2ce85ef225ae866b453a5a892ec4900
tusharjadwani <117591585+tusharjadwani@users.noreply.github.com>
2022-12-05 20:56:16 +0530

    up6

---
+++ src/components/Alert.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alert = () => {
+  return (
+    <div className='alert'>
+        File Uploaded
+    </div>
+  )
+}
+
+export default Alert
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,32 @@
 import React, { useRef } from 'react'
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Alert from './Alert';
 
 const Form = () => {
     const form = useRef(null)
     const url = process.env.URL || "https://gold-poised-fox.cyclic.app";
+    const [success, setSuccess] = useState(false)
+
+    setTimeout(() => {
+        setSuccess(false)
+    }, 2000);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const data = new FormData(form.current);
         e.target.reset();
 
-        await fetch(`${url}/api/upload`, {
+        fetch(`${url}/api/upload`, {
             method: 'post',
             body: data
-        })
+        }).then(res => res.json()).then(out => {setSuccess(out.name);})
+
     }
 
     return (
         <div>
+            {success && <Alert />}
             <header><Link to="/admin">Admin Panel</Link></header>
             <h1 >File Uploader</h1>
             <form className='container' ref={form} onSubmit={handleSubmit}>
